dcb49dac5e6bc0c39ad69decbc2f3ad5826544e9
Claudia Borghini <claudiaborghini@Claudias-MacBook-Air.local>
2020-07-30 20:20:21 +0200

    argument

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,17 +14,9 @@ class Cli
         self.traveller = Traveller.find_or_create_by(username: username)
         create_or_search_trip
     end
-        
 
-  
-
-    def create_or_search_trip
-        @response = prompt.select("Hello #{self.traveller.username}! What would you like to do?", %w(add_a_trip explore_a_trip?))
-        add_trip
-    end
-
-    def add_trip
-        if @response == "add_a_trip"
+    def add_trip(response = prompt.select("Hello #{self.traveller.username}! What would you like to do?", %w(add_a_trip explore_a_trip?)))
+        if response == "add_a_trip"
             new_trip = Trip.new(traveller: self.traveller)
             country = prompt.ask("Which country did you travel to?")
             city = prompt.ask("Nice! Which city did you go to?")
